6011ab7c3a6bb6751307086db026815e9be313c0
matan bakba <MatanBakba@gmail.com>
2021-02-18 19:21:40 +0200

    שגשגשג

--- convertListToTextFile.php
+++ convertListToTextFile.php
@@ -8,15 +8,15 @@ $userid = $_POST["UserId"];
 $allItems = getallItemsByListId($currentListId);
 
 $fileNewName = uniqid('', true).".txt";
-$myfile = fopen(__DIR__ ."uploads\\ListTextFiles\\".$fileNewName, "w");
+$myfile = fopen(__DIR__ ."uploads/ListTextFiles/".$fileNewName, "w");
 foreach($allItems as $Item){
     if($Item['type'] == "1"){
         fwrite($myfile, $Item['content']."\n");
     }
 }
 fclose($myfile);
-print(__DIR__ ."uploads\\ListTextFiles\\");
-$newList = new UserList(__DIR__ ."uploads\\ListTextFiles\\".$fileNewName."-".$currentListName.".txt", $userid, 2);
+print(__DIR__ ."uploads/ListTextFiles/");
+$newList = new UserList(__DIR__ ."uploads/ListTextFiles/".$fileNewName."-".$currentListName.".txt", $userid, 2);
 
 $newList->addList();
 ?>
